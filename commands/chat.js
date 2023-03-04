010147a87a0a709dc552a139072cde0d7961dfcf
James <biscxit@proton.me>
2023-03-03 16:52:34 -0800

    fix: disallow `/chat` in threads

--- commands/chat.js
+++ commands/chat.js
@@ -15,6 +15,17 @@ export const data = new SlashCommandBuilder()
   );
 
 export async function execute(interaction) {
+  const channel = interaction.channel;
+
+  if (channel.isThread()) {
+    await interaction.reply({
+      content: "You can't start a conversation in a thread!",
+      ephemeral: true,
+    });
+
+    return;
+  }
+
   const user = interaction.user;
   const message = interaction.options.getString('message');
 
@@ -25,7 +36,7 @@ export async function execute(interaction) {
 
   await interaction.reply({ embeds: [embed] });
 
-  const thread = await interaction.channel.threads.create({
+  const thread = await channel.threads.create({
     name: `💬 ${user.username} - ${limit(message, 50)}`,
     autoArchiveDuration: 60,
     reason: config.bot.name,
